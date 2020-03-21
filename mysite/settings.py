542d1b16f05f29347f6a504faa3daee7394cea63
Fabrina Purper <fabrina@posteo.de>
2020-03-21 16:54:56 +0100

    fixed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '04^9fik^jx#1(%ntq=ufb!&)%aeag3*!g5x%+mc=@fm(c^xxhe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'http://zuiu.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
